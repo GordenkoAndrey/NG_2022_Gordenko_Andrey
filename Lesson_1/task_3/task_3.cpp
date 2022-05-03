4c7c83f67645620d6df5575069a242664b626565
GordenkoAndrey <andreygordenko3002@gmail.com>
2022-05-03 03:52:15 +0300

    implemented a question-and-answer program for 3 task

---
+++ Lesson_1/task_3/task_3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age = 0;
+	int study = 0;
+
+	cout << "Enter your age: ";
+	cin >> age;
+
+	if (age > 10)
+	{
+		cout << "Do you study? (1/0) :";
+		cin >> study;
+
+		if (study == 0)
+		{
+			cout << "Why?" << endl;
+		}
+		else
+		{
+			cout << "You are awesome!" << endl;
+		}
+	}
+	else
+	{
+		cout << "Wow you are so big!" << endl;
+	}
+}
