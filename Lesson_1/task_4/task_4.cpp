78b8864249c0c8d586d12b18f4377ae3f87ae8ba
GordenkoAndrey <andreygordenko3002@gmail.com>
2022-05-03 12:13:16 +0300

    implemented a program for calculating the cost of components for 4 task

---
+++ Lesson_1/task_4/task_4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Motherboard = 0;
+    int Videocard = 0;
+    int CPU = 0;
+    double Discount = 0;
+    
+    cout << "Motherboard_price: ";
+    cin >> Motherboard;
+    cout << "Videocard_price: ";
+    cin >> Videocard;
+    cout << "CPU_price: ";
+    cin >> CPU;
+    cout << "Discount: ";
+    cin >> Discount;
+
+    cout << "Your PC will cost: " << (Motherboard + Videocard + CPU) - ((Discount / 100) * (Motherboard + Videocard + CPU)) << endl;
+}
