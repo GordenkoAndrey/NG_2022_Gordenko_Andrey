7ccaf77def123f5e06c94a7d3012ef7fd2a5be8b
GordenkoAndrey <andreygordenko3002@gmail.com>
2022-05-03 12:34:44 +0300

    implemented a program for outputting a square to the console for 1 task

---
+++ Lesson_2/task_1/task_1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int star = 0;
+
+	cout << "Enter the size of the square: ";
+	cin >> star;
+	cout << endl;
+
+	for (int i = 0; i < star; i++)
+	{
+		for (int j = 0; j < star; j++)
+		{
+			if (i != 0 && i != star - 1 && j != 0 && j != star - 1)
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << "*";
+			}
+		}
+		cout << endl;
+	}
+}
