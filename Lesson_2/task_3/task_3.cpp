54896b0dac4409bdc22b87a7b8c645dc1ca3dbdf
GordenkoAndrey <andreygordenko3002@gmail.com>
2022-05-03 12:51:41 +0300

    implemented a program to calculate the hash code of a number for 3 task

---
+++ Lesson_2/task_3/task_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 0;
+	int hash_code = 0;
+
+	cout << "Enter please number: ";
+	cin >> num;
+	cout << endl;
+	int num1 = num;
+	int last_num = num1 % 10;
+
+	while (num1 != 0)
+	{
+		if (num1 % 10 == num1 / 10 % 10)
+			hash_code += num1 % 10;
+
+		if (num1 < 10 && num1 == last_num)
+			hash_code += num1;
+		num1 /= 10;
+	}
+	cout << "Hash-code: " << hash_code << endl;
+}
