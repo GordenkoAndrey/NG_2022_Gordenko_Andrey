1f7c5b847ee9161a19842a10f0e6f8e0526951e1
GordenkoAndrey <andreygordenko3002@gmail.com>
2022-05-03 12:28:51 +0300

    implemented a program for solving quadratic equations for 5 task

---
+++ Lesson_1/task_5/task_5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int Discriminant = 0;
+	int x1 = 0;
+	int x2 = 0;
+
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter b: ";
+	cin >> b;
+	cout << "Enter c: ";
+	cin >> c;
+	cout << endl;
+
+	Discriminant = b * b - 4 * a * c;
+
+	if (Discriminant > 0) {
+		x1 = (-b + sqrt(Discriminant)) / (2 * a);
+		x2 = (-b - sqrt(Discriminant)) / (2 * a);
+		cout << "x1 = " << x1 << endl << "x2 = " << x2 << endl;
+	}
+	if (Discriminant == 0) {
+		x1 = (-b + sqrt(Discriminant)) / (2 * a);
+		cout << "x1 = " << x1 << endl;
+	}
+	if (Discriminant < 0) {
+		cout << "No roots!!!" << endl;
+	}
+}
