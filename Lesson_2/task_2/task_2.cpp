50edcf1d2d114221636f362610161d9899dcf99b
GordenkoAndrey <andreygordenko3002@gmail.com>
2022-05-03 12:44:58 +0300

    implemented a program to calculate the sum of paired digits in a number for 2 task

---
+++ Lesson_2/task_2/task_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 0;
+	int sum = 0;
+
+	cout << "Enter number: ";
+	cin >> num;
+	int num_1 = num;
+
+	for (int i = 0; num_1 > 0; i++)
+	{
+		if (num_1 % 10 % 2 == 0)
+			sum += num_1 % 10;
+		num_1 /= 10;
+	}
+	cout << "Sum = " << sum << endl;
+}
